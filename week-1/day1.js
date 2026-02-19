12012451dbfcd4f02b229d33d12370eb803f151a
buildwithaayush <ayushxdev347@gmail.com>
2026-02-19 19:38:07 +0530

    day1

---
+++ week-1/day1.js
@@ -0,0 +1,20 @@
+let user = "ayush";
+console.log(user)
+
+let $ = 1
+let _ = 27
+// $ and _ are only symbols we can use in declaring variable. 
+// even only these two symbols could be like variable name. 
+// as given above.
+console.log($ + _);
+
+let apple = "small apple"
+let APPLE = "CAPITAL APPLE"
+// declared variable name are case sensitive. hence apple and APPLE are two diffrent variables.
+console.log(apple)
+console.log(APPLE)
+
+// we cannot use reserved keywords like let , return etc. 
+
+num = 5
+console.log(num)
