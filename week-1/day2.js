a5ba8a145b78e423320260cb9b2263731b866e65
buildwithaayush <ayushxdev347@gmail.com>
2026-02-20 13:03:32 +0530

    Interaction: alert, prompt, confirm

--- week-1/day2.js
+++ week-1/day2.js
@@ -12,10 +12,31 @@ let phrase = `we welcome you ${str}`
 
 // Backticks are “extended functionality” quotes. They allow us to embed variables and expressions into a string by wrapping them in ${…}, for example:
 
-let age = null;
+let ag = null;
 //null data type means nothing, empty.
 
 let num;
 console.log(num)
 // type of returns the type of operands 
 console.log(typeof "0")
+
+// alerts 
+// alert is a modal window which shows a notification.
+
+//prompts 
+// prompts are modal window which accepts two arguments and where we can give back data also in input fields
+let age = prompt('how old are you?')
+alert(`you are ${age} old`)
+
+// confirm
+// confirm is also a modal window which ask a boolean question and results in either a true or false. 
+let isBoss = confirm('are you boss?')
+alert(isBoss)
+// We covered 3 browser-specific functions to interact with visitors:
+
+// alert
+// shows a message.
+// prompt
+// shows a message asking the user to input text. It returns the text or, if Cancel button or Esc is clicked, null.
+// confirm
+// shows a message and waits for the user to press “OK” or “Cancel”. It returns true for OK and false for Cancel/Esc.
