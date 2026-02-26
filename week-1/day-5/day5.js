0fb6bfec83784ec468551bccef9d07899661ae82
buildwithaayush <ayushxdev347@gmail.com>
2026-02-26 12:30:22 +0530

    Array

--- week-1/day-5/day5.js
+++ week-1/day-5/day5.js
@@ -123,23 +123,92 @@
 // 3. Create getMax(a, b, c) as arrow function
 // 4. Create reverseString(str) any way you want
 
- let isEven = (num) => {
-    if(num%2==0){
-        console.log('iseven')
-    }
-    else{
-        console.log('isodd')
-    }
- }
-isEven(8)
+//  let isEven = (num) => {
+//     if(num%2==0){
+//         console.log('iseven')
+//     }
+//     else{
+//         console.log('isodd')
+//     }
+//  }
+// isEven(8)
 
-let greet = function(first, last){
-  return `hello ${first} ${last}`
-}
-greet('ayush','singh');
+// let greet = function(first, last){
+//   return `hello ${first} ${last}`
+// }
+// greet('ayush','singh');
 
-let getmax = (a,b,c) => {
-    return b;
-}
+// let getmax = (a,b,c) => {
+//     return b;
+// }
 
+// day - 5 array and array method. 
+let arr = ['apple','banana','Orange',]
+// console.log(arr)
+// Methods pop/push , shift/unshift
+// push: appends an element to the end
+// shift: get an element from the beginning, advancing the queue,
+// so that the 2nd element becomes the 1st
+// arrays in js can work both as a queue and as a stack. they allow you
+// to add/ remove elements, both to/from the beginning or the end. 
 
+// pop 
+// Extracts the last element of the array and returns it 
+// console.log(arr.pop());
+
+// push 
+// append the elements to the end of the array
+// arr.push('Pear')
+// console.log(arr)
+
+// Methods that work with the beginning of the array: 
+
+// shift 
+// Extracts the first elemnt of the array and returns it 
+// arr.shift();
+// console.log(arr);
+
+// unshift 
+// Add the element of the beginning of the array
+// arr.unshift('kiwi')
+// console.log(arr)
+
+//Methods push and unshift can add multiple elements at once:
+// console.log(arr)
+// arr.push('grapes','avagandu','strawberry');
+// arr.unshift('berries','black grapes')
+// console.log(arr)
+// arr.age = 23;
+// console.log(arr);
+// console.log(arr.age)
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(`${i} is the index of ${arr[i]}`)
+// }
+// arr.shift()
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(`After shift method ${i} is the index of ${arr[i]}`)
+// }
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(`${i} is the index of ${arr[i]}`)
+// }
+// arr.pop() // pop is faster than shift/unshift as pop does not need to shift anything
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(`After pop method ${i} is the index of ${arr[i]}`)
+// }
+
+// for(let element of arr){ // for ..of loop for each element in the array
+//     console.log(element)
+// }
+
+// for (let key in arr){
+//     console.log(arr[key])
+// }
+
+// length 
+// let newarr = []
+// console.log(newarr.length)
+// console.log(newarr)
+
+// toString 
+// console.log(toString(arr))
