e00b1576c4dc34c36f33a7dee44c7f4f775629e4
buildwithaayush <ayushxdev347@gmail.com>
2026-04-07 08:03:50 +0530

    dsa questions

---
+++ DSA/day3.js
@@ -0,0 +1,49 @@
+// var isPalindrome = function(x) {
+//     let num = Array.from(String(x),Number)
+//     let start = 0; 
+//     let end = num.length-1;
+//     while(start < end){
+//         if(num[start] == num[end]){
+//             start++;
+//             end--;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// let x = 123;
+// let ans = isPalindrome(x);
+// console.log(ans)
+// let s =["H","a","n","n","a","h"]
+// function reverseString(s){
+
+
+// let start = 0 ;
+// let end = s.length -1;
+// while(start < end){
+//     let temp = s[start];
+//     s[start] = s[end];
+//     s[end] = temp;
+
+//     start++;
+//     end--;
+// }
+// }
+
+//  reverseString(s);
+//  console.log(s)
+
+// first unique character of a string
+let s = 'leetcode' 
+
+
+var firstUniqChar = function(s) {
+    let map = new Map();
+    for(let i = 0; i < s.length;i++){
+    map.set()
+    }
+};
+
