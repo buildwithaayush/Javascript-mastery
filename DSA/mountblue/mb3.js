5c14ec5c621a58c1c3631241b31842a2bb514185
buildwithaayush <ayushxdev347@gmail.com>
2026-04-14 15:24:01 +0530

    mountblue hackerrank problems

--- DSA/mountblue/mb3.js
+++ DSA/mountblue/mb3.js
@@ -113,23 +113,69 @@
 
 // }
 
-function bonAppetit(bill,k,b){
-  bill.splice(k,1);
-  let total = 0;
-  for(let i = 0 ; i < bill.length;i++){
-    total += bill[i];
-  }
-  let actualBill = total/2
-  let annaPaid = actualBill - b
-  if(annaPaid == 0){
-    return 'Bon Appetit'
-  }
-  else{
-    return annaPaid
-  }
-}
+// function bonAppetit(bill,k,b){
+//   bill.splice(k,1);
+//   let total = 0;
+//   for(let i = 0 ; i < bill.length;i++){
+//     total += bill[i];
+//   }
+//   let actualBill = total/2
+//   let annaPaid = actualBill - b
+//   if(annaPaid == 0){
+//     return 'Bon Appetit'
+//   }
+//   else{
+//     return annaPaid
+//   }
+// }
 
-let bills =[12,3,4]
-// bills.splice(1,1)
-// console.log(bills)
- bonAppetit(bills,1)
+// let bills =[12,3,4]
+// // bills.splice(1,1)
+// // console.log(bills)
+//  bonAppetit(bills,1)
+
+// function birthday(s,d,m){
+//   let count = 0;
+//  let start = 0;
+//  let end = m-1;
+//  while(end <= s.length){
+//     let total = 0;
+//   for(let i = start ; i <= end; i++){
+//     total += s[i];
+//   }
+//   if(total == d){
+//     count++;
+//   }
+//   start++;
+//   end++;
+//  }
+//  console.log()
+// }
+// let arr = [1,2,1,3,2];
+// let ans = birthday(arr,3,2);
+// console.log(ans)
+
+function Bird(arr){
+  let map = new Map();
+  for(let i = 0; i < arr.length ; i++){
+     map.set(arr[i],(map.get(arr[i])||0)+1)
+  }
+  let max = Number.MIN_VALUE
+  let result = Number.MIN_VALUE
+  for(let[key,value] of map){
+    if(value > max){
+      max = value
+      result = key;
+    }
+    else if(value == max){
+      if(key < result){
+         result = key;
+      }
+    }
+  }
+  return result;
+}
+// console.log(Number.MIN_VALUE)
+let arr = [1,2,2,2,2,3,3,5]
+let ans = Bird(arr)
+console.log(ans)
