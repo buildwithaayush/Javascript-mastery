bec5e53dc5652acd1c00cedbfb8497833c301cb0
buildwithaayush <ayushxdev347@gmail.com>
2026-04-14 23:32:35 +0530

    prototype and inheritance theory

--- week1/day4.js
+++ week1/day4.js
@@ -51,11 +51,166 @@
 
 // rabbit.walk();
 
-let user = {
-    name: 'john',
-    surname: 'smith',
-    set FullName(value){
-        [this.name,this.surname] = value.split(' ')
-    },
+// let user = {
+//     name: 'john',
+//     surname: 'smith',
+//     set FullName(value){
+//         [this.name,this.surname] = value.split(' ')
+//     },
    
+// }
+
+// const animal = {
+//     isAlive: 'true',
+//     walk(){
+       
+//     }
+// }
+
+// let rabbit = Object.create(animal)
+// rabbit.walk()
+// rabbit.eats = 'grass';
+// console.log(rabbit.eats)
+// console.log(rabbit.isAlive)
+// rabbit.walk = function(){
+//     console.log('rabbit is running')
+// }
+// rabbit.walk()
+
+
+// function Dog(name,breed){
+//     this.name = name;
+//     this.breed = breed;
+// }
+
+// // Methods go on the Prototype 0 shared by all instances 
+// Dog.prototype.bark = function(){
+//     console.log(this.name + ' is now barking louder !!!!!');
+// }
+
+// Dog.prototype.describe = function(){
+//     console.log(this.name + ' is a  '+ this.breed);
+// }
+
+// const d1 = new Dog('alex','Labrador')
+// const d2 = new Dog('rocky','pitbull')
+
+// d1.bark()
+// d2.describe()
+
+// let animal = {
+//     eats:true,
+//     food(){
+//         console.log(this.name + ' eats grass')
+//     }
+// }
+// function Rabbit(name){
+//     this.name = name;
+// }
+// Rabbit.prototype = animal;
+
+// let rabbit = new Rabbit('kaalu rabbit');
+// console.log(rabbit.eats)
+// rabbit.food()
+
+// Default F.prototype, constructor property
+// the default 'prototype' is an object with the only property constructor that points back to the 
+// function itself. 
+
+function Rabbit(){}
+/*
+default
+*/
+let rabbit = new Rabbit();
+
+// function Dog(name, breed) {
+//   this.name = name;     // Own property — each instance gets its own
+//   this.breed = breed;   // Own property
+// }
+
+// // Methods go on the PROTOTYPE — shared by all instances
+// Dog.prototype.bark = function() {
+//   console.log(this.name + " says Woof!");  // 'this' = the instance (Rule 2)
+// };
+
+// Dog.prototype.describe = function() {
+//   console.log(this.name + " is a " + this.breed);
+// };
+
+// const d1 = new Dog("Bruno", "Labrador");
+// const d2 = new Dog("Rocky", "German Shepherd");
+
+// d1.bark(); // "Bruno says Woof!" — method found on Dog.prototype
+// d2.bark(); // "Rocky says Woof!" — SAME function, different 'this'
+
+// // Both share the SAME bark function:
+// console.log(d1.bark === d2.bark); // true! Memory efficient.
+
+// // The chain: d1 → Dog.prototype → Object.prototype → null
+
+class Dog{
+    constructor(name,breed){
+        this.name = name;
+        this.breed = breed;
+    }
+
+    bark(){  // Goes on Dog.prototype automatically
+        console.log(this.name + 'says woof!')
+    }
+
+    describe(){
+        console.log(this.name +  ' is a ' + this.breed)
+    }
+
 }
+
+// Inheritance with extends
+class GuideDog extends Dog{
+    constructor(name,breed,owner){
+        super(name,breed);
+        this.owner = owner;
+    }
+
+    guide(){
+        console.log(this.name + ' guides ' + this.owner);
+    }
+
+}
+
+const g = new GuideDog('buddy','Golden Retriever','Ayush');
+// g.bark();
+// g.guide();
+// g.describe();
+
+class MyClass {
+    constructor(){}
+    method1(){}
+    method2(){} 
+}
+
+// class User {
+//     constructor(name){
+//         this.name = name;
+//     }
+//     sayHi(){
+//         console.log('welcome ' + this.name)
+//     }
+// }
+
+// let ayush = new User('Ayush');
+// ayush.sayHi()
+// console.log(typeof User)
+
+// what is a class ?
+// In javascript class is a type of function.
+
+class User {
+    constructor(name){
+        this.name = name
+    }
+}
+
+console.log(typeof User)// class is a function 
+
+alert(User === User.prototype.constructor);
+
