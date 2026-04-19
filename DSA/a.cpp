c0dc29cac0fd997ce37f1fe9145c8c77ea37148d
buildwithaayush <ayushxdev347@gmail.com>
2026-04-19 13:00:14 +0530

    started with day6

---
+++ DSA/a.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() 
+{
+    // int, float, double;
+    // int 32 bits values
+    // long long int 64 = 2^64
+    // stl standard template lbrary
+    // map red - back trees -> avoid colisions -> log(sizeofmap)
+    // unordered map hashmap best case O(1) worst case O(n)
+    // set sorted order sare distinct values ko store krta hai -> 1,2,3,4,5,6 no duplication
+    // multiset sorted order mein saare values ko store-> 1,1,2,3,3,3,4,4 duiplication allowed
+    // stack // priortiy queues, monotonic stacki
+    // binary search 
+    // ternary search
+    // recursion -> at least a month
+    // graph -> dfs and bfs // multisource bfs
+    // trees// dynamic programming
+    // dijkstra 
+    // graph algorithms floyd warshall, other things
+    // dp
+    // trie data structure
+    // string algorithms -> rabin karp, rolling hash, and others
+    // mcm dp
+    // mathematics like josephus problem
+}
---
+++ DSA/day6.js
@@ -0,0 +1,46 @@
+// // problem group anagram 
+// let strs = ["eat","tea","tan","ate","nat","bat"]
+// let sortedag = []
+// for(let i = 0 ; i < strs.length ; i++){
+//     let key = strs[i].split('').sort().join('');
+//     if(! map.has(key)){
+//         map.set(key,[strs[i]]);
+//     }
+//     else{
+//         map.get(key).push(strs[i]);
+//     }
+
+//     return [...map.values()]
+
+// }
+
+
+
+
+
+
+// let str = 'ayush';
+// console.log(str.split('').sort().join(''))
+
+
+// top k frequency
+// let arr = [1,2,1,2,1,4,5,2,2,2]
+// function frequency(arr){
+//     let map = new Map()
+//     for(let i = 0 ; i < arr.length ; i++){
+//         map.set(arr[i], (map.get(arr[i])||0)+1);
+//     }
+//     let freq = []
+//     for(let [key,value] of map){
+//         freq.push([key,value])
+//     }
+
+//    // suppose freq is sorted by count 
+
+
+// }
+
+// let ans = frequency(arr);
+// console.log(ans)
+
+ freq.sort((a,b)=>(b))
