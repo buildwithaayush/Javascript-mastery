f2e7a06aaf7609b9ae2e732491b0ad5beb387682
buildwithaayush <ayushxdev347@gmail.com>
2026-04-26 00:21:54 +0530

    caesar cipher

---
+++ week2/day2.js
@@ -0,0 +1,23 @@
+// eventListners 
+
+// function sayHello(){
+//     let body = document.body;
+//     let p = document.createElement('p');
+//     p.textContent = 'Hello'
+//     body.appendChild(p)
+// }
+
+// const btn = document.querySelector("button");
+
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+
+// function changeBackground() {
+//   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+//   document.body.style.backgroundColor = rndCol;
+// }
+
+// btn.addEventListener("click", changeBackground); 
+// btn.addEventListener('click',sayHello)
+
